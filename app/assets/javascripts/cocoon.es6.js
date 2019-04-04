feeca5654281253649de2ccb6753246907dfbdcc
Youri van der Lans <youri@itflows.nl>
2019-04-04 17:18:05 +0200

    Ensure _destroy is filled with ‘1’.
    toRemove contains the entire element to be removed.

--- app/assets/javascripts/cocoon.es6.js
+++ app/assets/javascripts/cocoon.es6.js
@@ -158,7 +158,7 @@ class Cocoon {
       if (removeLink.classList.contains('dynamic'))
         container.removeChild(toRemove)
       else {
-        var input = toRemove.previousSibling
+        var input = removeLink.previousSibling
         if (input.matches('input[type="hidden"]'))
           input.value = '1'
 
